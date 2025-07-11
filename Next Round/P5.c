16f6a2bc38b32de15d4a4af1312469e7474fdf7b
Sajid Ahsan <sajidahsan.05@gmail.com>
2025-07-12 01:42:37 +0600

    Update P5.c

--- Next Round/P5.c
+++ Next Round/P5.c
@@ -3,10 +3,13 @@ int n,k;
 int count=0;
 void eligible(int array[])
 {
+//This loop will take n participants score
     for(int i=0; i<n; i++)
     {
         scanf("%d",&array[i]);
     }
+/* Comparing all participant's score respect to kth position participant's score 
+if found then count it */
         for(int i=0; i<n; i++)
         {
              if(array[k-1]<=array[i] && array[i]>0)
@@ -21,6 +24,7 @@ int main()
 {
     scanf("%d",&n);
     scanf("%d",&k);
+//Checking n and k's value is positive or not and k is greater than n or not.
     if(!(n>=k && n>=1 && n<=100 && n>=1 && n<=100))
     {
         scanf("%d",&n);
