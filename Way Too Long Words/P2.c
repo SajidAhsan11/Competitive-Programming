096e2df2ff41bab13958bd8e849ccda26ec7682d
Sajid Ahsan <sajidahsan.05@gmail.com>
2025-07-12 02:45:35 +0600

    Intregate Comments P2.c

--- Way Too Long Words/P2.c
+++ Way Too Long Words/P2.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<string.h>
 
+/* This function will check is a string more than 10 character if yes then replace it by character number between first letter and last latter middle of first and last letter */
+
 void check(char name[100])
 {
     if(strlen(name)>10)
@@ -16,6 +18,9 @@ int main()
     int n;
     char name[100];
     scanf("%d",&n);
+
+//This loop will take n strings and check
+
     for(int i=0; i<n; i++)
     {
         scanf("%s",name);
