6c6f183a801fc915bab3fc22ba701e941892f9ca
Sajid Ahsan <sajidahsan.05@gmail.com>
2025-08-15 23:37:40 +0600

    Comment added

--- Word Capitalization/Code.cpp
+++ Word Capitalization/Code.cpp
@@ -4,10 +4,12 @@ int main()
 {
     string word;
     cin>>word;
+    // Changing the 1st character lowercase letter to upercase letter 
     word[0]=toupper(word[0]);
     for(auto i : word)
     {
         cout<<i;
     }
     return 0;
+
 }
