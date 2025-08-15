68cd351ff5b6ba39483be6766e91c2dc03b61aa2
Sajid Ahsan <sajidahsan.05@gmail.com>
2025-08-15 23:54:42 +0600

    Comment added

--- Bear and Big Brother/Code.cpp
+++ Bear and Big Brother/Code.cpp
@@ -5,6 +5,7 @@ int main()
     int a,b;
     int count=0;
     cin>>a>>b;
+    // Finfing in how many year a becomes greater then b. (In a year a becomes triple and b becomes double)
     while(true)
     {
         a*=3;
@@ -17,4 +18,5 @@ int main()
     }
     cout<<count<<endl;
     return 0;
+
 }
